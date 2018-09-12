6290f21e0d9d76ace4547c870daa4186b5b505a7
valentichu <valentichu@foxmail.com>
2018-09-12 17:35:47 +0800

    修正图标显示问题

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+    chainWebpack: config => {
+        // 设置默认svg loader不包含icon文件夹
+        config.module.rule('svg')
+            .exclude
+              .add(path.resolve(__dirname, 'src', 'icons'))
+              .end()
+
+        // 添加icon loader
+        config.module.rule('icon')
+            .include
+              .add(path.resolve(__dirname, 'src', 'icons'))
+              .end()
+            .test(/\.(svg)(\?.*)?$/)
+            .use('svg-sprite-loader')
+            .loader('svg-sprite-loader')
+            .options({ symbolId: 'icon-[name]' })
+    }
+}
